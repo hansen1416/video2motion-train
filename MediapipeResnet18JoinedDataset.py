efe9773e70f677937c50e8dc7fd722d448458645
Hanson <105476@grundfos.com>
2024-03-05 11:09:16 +0800

    1

---
+++ MediapipeResnet18JoinedDataset.py
@@ -0,0 +1,73 @@
+import os
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class MediapipeResnet18JoinedDataset(Dataset):
+
+    def __init__(
+        self,
+        mediapipe_joined_dir,
+        resnet_joined_dir,
+        anim_euler_joined_dir,
+    ) -> None:
+        super().__init__()
+
+        self.mediapipe_joined_dir = mediapipe_joined_dir
+        self.resnet_joined_dir = resnet_joined_dir
+        self.anim_euler_joined_dir = anim_euler_joined_dir
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.mediapipe_joined = np.load(
+            os.path.join(self.mediapipe_joined_dir, "joined.npy")
+        )
+        self.resnet_joined = np.load(os.path.join(self.resnet_joined_dir, "joined.npy"))
+
+        self.anim_euler_joined = np.load(
+            os.path.join(self.anim_euler_joined_dir, "joined.npy")
+        )
+
+        assert (
+            self.mediapipe_joined.shape[0] == self.resnet_joined.shape[0]
+            and self.resnet_joined.shape[0] == self.anim_euler_joined.shape[0]
+        ), "The number of samples in the datasets are not equal."
+
+    def __len__(self) -> int:
+        return self.anim_euler_joined.shape[0]
+
+    def __getitem__(self, idx):
+        return (
+            torch.tensor(self.mediapipe_joined[idx], dtype=torch.float32).to(
+                self.device
+            ),
+            torch.tensor(self.resnet_joined[idx], dtype=torch.float32).to(self.device),
+            torch.tensor(self.anim_euler_joined[idx], dtype=torch.float32).to(
+                self.device
+            ),
+        )
+
+
+if __name__ == "__main__":
+
+    from torch.utils.data import DataLoader
+
+    from constants import MEDIAPIPE_JOINED_DIR, RESNET_JOINED_DIR, ANIM_EULER_JOINED_DIR
+
+    ds = MediapipeResnet18JoinedDataset(
+        MEDIAPIPE_JOINED_DIR, RESNET_JOINED_DIR, ANIM_EULER_JOINED_DIR
+    )
+
+    print(len(ds))
+    print(ds[0])
+
+    dl = DataLoader(ds, batch_size=32, shuffle=True)
+
+    print(len(dl))
+
+    for i, (mediapipe, resnet, anim_euler) in enumerate(dl):
+        pass
+
+    print(i)
--- constants.py
+++ constants.py
@@ -6,6 +6,31 @@ TRAIN_DATASET_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "train")
 
 TEST_DATASET_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "test")
 
+
+MEDIAPIPE_QUEUE_DATA_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "queue_data", "mediapipe"
+)
+
+RESNET_QUEUE_DATA_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "queue_data", "resnet"
+)
+
+ANIM_EULER_QUEUE_DATA_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "queue_data", "anim_euler"
+)
+
+MEDIAPIPE_JOINED_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "joined", "mediapipe"
+)
+
+RESNET_JOINED_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "joined", "resnet"
+)
+
+ANIM_EULER_JOINED_DIR = os.path.join(
+    HOME_DIR, "Documents", "video2motion", "joined", "anim_euler"
+)
+
 if __name__ == "__main__":
 
     for f in os.listdir(TRAIN_DATASET_DIR):
