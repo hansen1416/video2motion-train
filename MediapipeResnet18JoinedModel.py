9032e4a3d33c314961f26d0c19741727a8c51f6e
Hanson <105476@grundfos.com>
2024-03-05 17:48:22 +0800

    1

---
+++ MediapipeResnet18JoinedModel.py
@@ -0,0 +1,65 @@
+import numpy as np
+import torch
+from torch import Tensor
+from torch import nn
+
+
+class WeightedCombineLayer(nn.Module):
+    def __init__(self):
+        super(WeightedCombineLayer, self).__init__()
+        # Initialize weights with 0.5
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Initialize weights with 0.5
+        self.weight_inp1 = torch.nn.Parameter(torch.full((1, 66), 0.5)).to(device)
+        self.weight_inp2 = torch.nn.Parameter(torch.full((1, 66), 0.5)).to(device)
+
+    def forward(self, input1, input2):
+        return input1 * self.weight_inp2 + input2 * self.weight_inp2
+
+
+class MediapipeResnet18JoinedModel(nn.Module):
+    def __init__(self) -> None:
+        super(MediapipeResnet18JoinedModel, self).__init__()
+
+        self.res_dp1 = nn.Dropout(0.25)
+        self.res_fc1 = nn.Linear(512, 256)
+        self.res_dp2 = nn.Dropout(0.25)
+        self.res_fc2 = nn.Linear(256, 128)
+        self.res_dp3 = nn.Dropout(0.25)
+        self.res_fc3 = nn.Linear(128, 66)
+
+        self.mp_fc1 = nn.Linear(99, 128)
+        self.mp_fc2 = nn.Linear(128, 256)
+        self.mp_fc3 = nn.Linear(256, 128)
+        self.mp_fc4 = nn.Linear(128, 66)
+
+    def forward(self, mediapipe_input, resnet_input):
+
+        mediapipe_input = self.mp_fc1(mediapipe_input)
+        mediapipe_input = nn.functional.tanh(mediapipe_input)
+        mediapipe_input = self.mp_fc2(mediapipe_input)
+        mediapipe_input = nn.functional.tanh(mediapipe_input)
+        mediapipe_input = self.mp_fc3(mediapipe_input)
+        mediapipe_input = nn.functional.tanh(mediapipe_input)
+        mediapipe_input = self.mp_fc4(mediapipe_input)
+        # now the shape pf mediapipe_input is (batch_size, 66)
+
+        resnet_input = self.res_dp1(resnet_input)
+        resnet_input = self.res_fc1(resnet_input)
+        resnet_input = nn.functional.tanh(resnet_input)
+        resnet_input = self.res_dp2(resnet_input)
+        resnet_input = self.res_fc2(resnet_input)
+        resnet_input = nn.functional.tanh(resnet_input)
+        resnet_input = self.res_dp3(resnet_input)
+        resnet_input = self.res_fc3(resnet_input)
+        # clamp the values to be between -pi and pi
+        resnet_input = torch.clamp(resnet_input, -np.pi, np.pi)
+        # now the shape of resnet_input is (batch_size, 66)
+
+        # apply the weighted combine layer, the output shape is (batch_size, 66)
+        combined_output = WeightedCombineLayer()(mediapipe_input, resnet_input)
+
+        # Reshape to (batch_size, 22, 3), so that we can use it as input to the loss function
+        output = combined_output.reshape(-1, 22, 3)
+
+        return output
--- predict.py
+++ predict.py
@@ -1,24 +1,22 @@
 import os
 import json
 
-import oss2
 import numpy as np
 import torch
-from torch import Tensor
-from torch import nn
-from torch.utils.data import DataLoader
 
-from MediapipeTransferLinear import MediapipeTransferLinear
-from oss2_utils import get_bucket
+
+from MediapipeResnet18JoinedModel import MediapipeResnet18JoinedModel
 
 if __name__ == "__main__":
 
-    saved_model_path = os.path.join("models", "model.pth")
+    saved_model_path = os.path.join("models", "model_24.pth")
 
     # load model saved by `torch.save(model.state_dict(), "model.pth")`
-    model = MediapipeTransferLinear()
+    model = MediapipeResnet18JoinedModel()
 
-    model.load_state_dict(torch.load(saved_model_path))
+    model.load_state_dict(
+        torch.load(saved_model_path, map_location=torch.device("cpu"))
+    )
 
     model.eval()  # set the model to evaluation mode
 
@@ -32,39 +30,40 @@ if __name__ == "__main__":
         f"mediapipe/{humanoid_name}/{animation_name}/{elevation}/{azimuth}/"
     )
 
-    # ger all mediapipe prediected result from oss for a given animation
-    bucket = get_bucket()
+    print(model)
 
-    world_landmarks_json = {}
+    # # ger all mediapipe prediected result from oss for a given animation
 
-    # list all files in the oss folder
-    for obj in oss2.ObjectIterator(bucket, prefix=mediapipe_path):
-        # if obj.key.endswith("world_landmarks.json"):
-        if obj.key.endswith("world_landmarks.json"):
+    # world_landmarks_json = {}
 
-            # use regexp to get the n_frame
-            n_frame = int(obj.key.split("/")[-2])
+    # # list all files in the oss folder
+    # for obj in oss2.ObjectIterator(bucket, prefix=mediapipe_path):
+    #     # if obj.key.endswith("world_landmarks.json"):
+    #     if obj.key.endswith("world_landmarks.json"):
 
-            world_landmarks_json[n_frame] = obj.key
-            # world_landmarks = json.loads(obj.read())
-            # landmarks1d = get_landmarks1d(world_landmarks)
+    #         # use regexp to get the n_frame
+    #         n_frame = int(obj.key.split("/")[-2])
 
-            # # predict inputs[0] using the model
-            # with torch.no_grad():  # no need to track the gradients
-            #     prediction = model(landmarks1d)
+    #         world_landmarks_json[n_frame] = obj.key
+    #         # world_landmarks = json.loads(obj.read())
+    #         # landmarks1d = get_landmarks1d(world_landmarks)
 
-        # print(obj.key)
+    #         # # predict inputs[0] using the model
+    #         # with torch.no_grad():  # no need to track the gradients
+    #         #     prediction = model(landmarks1d)
 
-    features = []
+    #     # print(obj.key)
 
-    # sort world_landmarks_json
-    for _, v in sorted(world_landmarks_json.items()):
+    # features = []
 
-        # todo load the world_landmarks_json from oss
-        landmarks_obj = bucket.get_object(v)
+    # # sort world_landmarks_json
+    # for _, v in sorted(world_landmarks_json.items()):
 
-        world_landmarks = json.loads(landmarks_obj.read())
+    #     # todo load the world_landmarks_json from oss
+    #     landmarks_obj = bucket.get_object(v)
 
-    # # predict inputs[0] using the model
-    # with torch.no_grad():  # no need to track the gradients
-    #     prediction = model(inputs)
+    #     world_landmarks = json.loads(landmarks_obj.read())
+
+    # # # predict inputs[0] using the model
+    # # with torch.no_grad():  # no need to track the gradients
+    # #     prediction = model(inputs)
